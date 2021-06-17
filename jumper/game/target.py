13159cc32d0da5caa340e99c14d2fed802a5dc66
ctfuller98 <ful17005@byui.edu>
2021-06-17 16:21:12 -0600

    target first upload

--- jumper/game/target.py
+++ jumper/game/target.py
@@ -1,2 +1,11 @@
+import random
 class Target: 
-    pass
+    '''This class holds a list of words and chooses one to use for the game.
+    Attributes: word_list (list)
+    '''
+    def __init__(self):
+        self.word_list =  ["happy", "behind", "jump", "town", "mend", "amazing", "terrify","afternoon", "poor", "shrill"]
+    def get_word(self, word_list):
+        game_word = random.choice(word_list)
+        print(f"The word is {game_word}")
+        return game_word
