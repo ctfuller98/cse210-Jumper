490062cd405e79e8c0e0724bdf96b45ccee8cf66
ctfuller98 <ful17005@byui.edu>
2021-06-17 16:29:24 -0600

    sabetour beginning comments

---
+++ jumper/game/sabetour.py
@@ -0,0 +1,5 @@
+class Sabetour:
+    '''This class is responsible for 'cutting the jumper's chute'. He keeps track 
+    of the letters that the jumper has guessed and he displays the results. '''
+    '''Attributes: letters_guessed (list) -- A way to store what letters have been guessed
+    Methods: display_results'''
