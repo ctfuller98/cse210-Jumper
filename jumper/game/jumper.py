e310f29ea2080d456495c1edbdebed2ab8155809
Braxton Meyer <braxtonlmeyer@gmail.com>
2021-06-17 16:24:22 -0600

    moved jumper to it's proper place

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,2 +1,13 @@
-class Jumper: 
-    pass
+
+class jumper:
+    '''
+    Represents the player input in the game. 
+    Responsible for parsing input from the user, which letter they are guessing.
+    '''
+    def __init__(self):
+        #If we need anything in here, this will be the place for initialization
+        pass
+
+    def get_letter(self):
+        guess = input("Guess a letter [a-z]: ")
+        return guess
