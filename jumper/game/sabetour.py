20b676d6fdde377c36614616a96aaf0f99342ae0
ctfuller98 <ful17005@byui.edu>
2021-06-23 17:24:30 -0600

    added skeleton of sabetour

--- jumper/game/sabetour.py
+++ jumper/game/sabetour.py
@@ -3,3 +3,19 @@ class Sabetour:
     of the letters that the jumper has guessed and he displays the results. '''
     '''Attributes: letters_guessed (list) -- A way to store what letters have been guessed
     Methods: display_results'''
+    def __init__(self):
+        self.letters_guessed = []
+        update_list()
+        display_results()
+    def update_list(self, letters_guessed):
+        
+    def display_results(self):
+        print("_ _ _ _ _")
+        print(" ___  ")
+        print("/___\ " )       
+        print("\   /")      
+        print(" \ / ")         
+        print("0")  
+        print(" /|\  ")        
+        print("/ \ ") 
+        print("^^^^^^^")         
